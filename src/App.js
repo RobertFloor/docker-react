43eec39959fa4698b2a2ee5ddfc258d2f7b76a21
Robert Floor <robertfloor@MacBook-Pro-van-Robert.local>
2020-11-12 14:33:44 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! Welcome to react. See you later, I was chaged!
+          Hi there! Welcome to react. I was changed at the feature branch!
         </p>
         <a
           className="App-link"
